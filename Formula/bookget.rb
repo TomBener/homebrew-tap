37d471d8c996179fefcd841dbd02dea575e561a7
TomBen <mr.ren.tao123@gmail.com>
2024-04-05 19:30:04 +0800

    Update version to bookget 24.0331

--- Formula/bookget.rb
+++ Formula/bookget.rb
@@ -5,7 +5,7 @@ class Bookget < Formula
   desc "数字古籍图书下载工具"
   homepage "https://github.com/deweizhu/bookget"
   license "GPL-3.0"
-  version "24.0116"
+  version "24.0331"
 
   livecheck do
     url :url
@@ -14,10 +14,10 @@ class Bookget < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/deweizhu/bookget/releases/download/#{version}/bookget-#{version}.macOS.tar.bz2"
-    sha256 "5a7237e5637b24329aef75f04b5d1e1ded61608c18b8386e70f606cce9f2548e"
+    sha256 "90d2675a07e87e9c2ac2712626f72850fdf0ba4d716ee941801040f8d7c8b9db"
   else
     url "https://github.com/deweizhu/bookget/releases/download/#{version}/bookget-#{version}.macOS-arm64.tar.bz2"
-    sha256 "acff12c9cda8d0110352c1e759a54a58fbe50573b9c2ce45819ada68095a55bc"
+    sha256 "80c733ffb5bc816f72f5e3043055f7ab59b6ed42271804b8e38233eb65dd8c26"
   end
 
   def install
