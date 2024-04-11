3ab530ffe06937b52fb2d5b307bbb5d7ae52062b
TomBen <mr.ren.tao123@gmail.com>
2024-04-11 08:37:26 +0800

    Update quarto-prerelease.rb

--- Casks/quarto-prerelease.rb
+++ Casks/quarto-prerelease.rb
@@ -1,7 +1,7 @@
 # Formula code for installing the prerelease of Quarto
 
 cask "quarto-prerelease" do
-  version "1.5.28"
+  version "1.5.29"
   sha256 :no_check
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
@@ -20,11 +20,11 @@ cask "quarto-prerelease" do
   pkg "quarto-#{version}-macos.pkg"
 
   # Create a symlink for Typst
-  # if Hardware::CPU.intel?
-  #   binary "#{appdir}/quarto/bin/tools/x86_64/typst"
-  # else
-  #   binary "#{appdir}/quarto/bin/tools/aarch64/typst"
-  # end
+  if Hardware::CPU.intel?
+    binary "#{appdir}/quarto/bin/tools/x86_64/typst"
+  else
+    binary "#{appdir}/quarto/bin/tools/aarch64/typst"
+  end
 
   uninstall pkgutil: "org.rstudio.quarto"
 
