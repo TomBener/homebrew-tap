d4b5acfd17f22067bdcfa3282f47ad6bbc554afd
TomBen <mr.ren.tao123@gmail.com>
2024-10-22 08:25:19 +0800

    Update quarto-prerelease.rb

--- Casks/quarto-prerelease.rb
+++ Casks/quarto-prerelease.rb
@@ -1,7 +1,7 @@
 # Formula code for installing the prerelease of Quarto
 
 cask "quarto-prerelease" do
-  version "1.6.28"
+  version "1.6.29"
   sha256 :no_check
 
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
