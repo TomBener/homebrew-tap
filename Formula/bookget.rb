00e20c1c3fff3d023defe366a00edb6358ce5654
TomBen <mr.ren.tao123@gmail.com>
2024-11-20 21:28:02 +0800

    Update bookget.rb

--- Formula/bookget.rb
+++ Formula/bookget.rb
@@ -5,7 +5,7 @@ class Bookget < Formula
   desc "数字古籍图书下载工具"
   homepage "https://github.com/deweizhu/bookget"
   license "GPL-3.0"
-  version "24.0923"
+  version "24.1118"
 
   livecheck do
     url :url
