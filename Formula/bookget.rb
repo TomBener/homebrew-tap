dead5fcbb75c77ae6edbc9edd3dd361f6b4b3592
TomBen <mr.ren.tao123@gmail.com>
2024-11-21 08:20:50 +0800

    Update bookget.rb

--- Formula/bookget.rb
+++ Formula/bookget.rb
@@ -14,10 +14,10 @@ class Bookget < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/deweizhu/bookget/releases/download/#{version}/bookget-#{version}.macOS.tar.bz2"
-    sha256 :no_check
+    sha256 "b5fbbcd8ad35d093ef0ff0944e43848eb4f9d1f7be330433707b103ba79dbd89"
   else
     url "https://github.com/deweizhu/bookget/releases/download/#{version}/bookget-#{version}.macOS-arm64.tar.bz2"
-    sha256 :no_check
+    sha256 "bc2a7782f1728b0f09bea796c9d83f041748daf5649d2c375393bfc112475653"
   end
 
   def install
