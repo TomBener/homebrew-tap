3e87cbe5a50bcf2fecec80ea3e07fbaf6c2ad2e6
TomBen <mr.ren.tao123@gmail.com>
2024-12-20 09:00:11 +0800

    Update searchlink.rb

--- Casks/searchlink.rb
+++ Casks/searchlink.rb
@@ -1,6 +1,6 @@
 cask "searchlink" do
-    version "2.3.76"
-    sha256 "2241b910f141da82678ca759231157373032e3985d1648e2b4aada0014ba8f20"
+    version "2.3.80"
+    sha256 "1fec1cf0d6c1eba7bd84bfda7bd97f15b8447e4414d2277812f3348bbe3634fd"
 
     url "https://github.com/ttscoff/searchlink/releases/download/#{version}/SearchLink.zip"
     name "SearchLink"
