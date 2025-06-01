d8efcbe03ad3ea1b8f5e63e4a8fc556d30ca734f
TomBen <mr.ren.tao123@gmail.com>
2025-06-01 14:55:00 +0800

    Update version prefix

--- Casks/font-kaiti-gb2312.rb
+++ Casks/font-kaiti-gb2312.rb
@@ -2,7 +2,7 @@ cask "font-kaiti-gb2312" do
   version "1.0"
   sha256 "faa9b16c38dba8da9a8b43e488b2cc2f6ddfb1106231367d331a30046a8da718"
 
-  url "https://github.com/TomBener/homebrew-tap/releases/download/v#{version}/Kaiti_GB2312.ttf"
+  url "https://github.com/TomBener/homebrew-tap/releases/download/#{version}/Kaiti_GB2312.ttf"
   name "Fangzheng Kaiti GB2312"
   desc "Legacy Chinese font called Kaiti for the GB2312 encoding"
   homepage "https://github.com/TomBener/homebrew-tap"
